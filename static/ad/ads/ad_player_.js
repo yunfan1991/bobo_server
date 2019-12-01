8766a423c193d7c4f681492c7e7641d7921a8574
georong <mfqb@qq.com>
2019-12-01 23:29:19 +0800

    add ads

---
+++ static/ad/ads/ad_player_.js
@@ -0,0 +1,2 @@
+document.write( '<a href=\"https://u.jd.com/np0LUL\" target=\"_blank\"><img  width=\"760\" src=\"http://ads.wulibobo.com/ads/images/jkbc_760.png\"></a>\n' );
+
---
+++ static/ad/ads/player_pause_normal.js
@@ -0,0 +1,1 @@
+document.write( '<a href=\"https://u.jd.com/np0LUL\" target=\"_blank\"><img width=\"400\" height=\"400\" src=\"http://ads.wulibobo.com/ads/images/jkcxbc_0.jpg\"></a>\n' );
